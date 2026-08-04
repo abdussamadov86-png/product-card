7d39b0f679014305285409106d133c0f6bdd2eae
Akhmad_2706 <abdussamado86@gmail.com>
2026-08-05 00:45:18 +0300

    Completed all 7 points of the homework assignment and re-watched the video lesson.

---
+++ homework-7.js
@@ -0,0 +1,56 @@
+//Вывод в консоль лог: город и температуру
+function infTemperCity(city, temperature) {
+  console.log(`Сейчас в городе ${city} температура — ${temperature} градусов по Цельсию`);
+}
+infTemperCity('Махачкала', 36);
+
+//Вывод в консоль лог: по заднному параметру вывод скорости света
+const SPEEDLIGHT = 299792;
+function checkSpeed(speed) {
+  if (speed > SPEEDLIGHT) {
+    console.log('Сверхсветовая скорость');
+  } else if (speed === SPEEDLIGHT) {
+    console.log('Скорость света');
+  } else {
+    console.log('Субсветовая скорость');
+  }
+}
+checkSpeed(390000);
+checkSpeed(90000);
+checkSpeed(299792);
+
+//Вывод в консоль лог: покупка синхрофазотрона
+const productName = 'Синхрофазотрон ОИЯИ';
+let productPrice = 2_500_000;
+
+function buyProduct(myBalance) {
+  if (productPrice <= myBalance) {
+    console.log(`${productName} приобретен. Спасибо за покупку!`);
+  } else {
+    const difference = productPrice - myBalance;
+    console.log(`Вам не хватает ${difference}$, пополните баланс`);
+  }
+}
+buyProduct(250_000);
+buyProduct(2_500_000);
+
+//Функция выбора любимой марки авто (Создать 1 функцию и именовать её по своему усмотрению)
+
+const myFavoriteCar = 'ZEEKR';
+function buyThings(car) {
+  if (car === myFavoriteCar) {
+    console.log('Беру, мой любимый авто');
+  } else {
+    console.log('Нет, спасибо.');
+  }
+}
+buyThings('ZEEKR');
+buyThings('another');
+
+//Создать 3 переменных
+
+let grandmotherName = 'Чакар';
+
+const g_mBirthDay = 1378;
+
+var myFavoriteFruit = 'apple';
