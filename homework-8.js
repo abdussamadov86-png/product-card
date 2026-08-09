f57088de9769956a0efaec3bde5d134e4d620798
Akhmad_2706 <abdussamado86@gmail.com>
2026-08-09 04:09:11 +0300

    fixed the formatting

--- homework-8.js
+++ homework-8.js
@@ -1,43 +1,44 @@
-// Создайте объект на основе ваших данных
+
+  // Создайте объект на основе ваших данных
 
 const userInfo = {
-  name: "Мурад",
-  surname: "Абдулсамедов",
-  eMail: "abdussamadov86@gmail.com",
-  job: "teacher",
-  age: 38,
-  city: "Makhachkala",
-  relationshipStatus: "married",
-}
-console.log(userInfo)
+    name: "Мурад",
+    surname: "Абдулсамедов",
+    eMail: "abdussamadov86@gmail.com",
+    job: "teacher",
+    age: 38,
+    city: "Makhachkala",
+    relationshipStatus: "married",
+};
+console.log(userInfo);
 
 // Создайте объект данных об автомобиле
 
 const carInfo = {
-  carMake: "ZEEKR",
-  model: "7x",
-  manufactureYear: 2026,
-  color: "black",
-  transmission: "single-stage reduction gear",
-}
+    carMake: "ZEEKR",
+    model: "7x",
+    manufactureYear: 2026,
+    color: "black",
+    transmission: "single-stage reduction gear",
+};
 
 // дополнительное свойство - владелец авто
 
-carInfo.owner = userInfo
+carInfo.owner = userInfo;
 
-console.log(carInfo)
+console.log(carInfo);
 
 
 // функция принимающая аргументом объект, описанный в пункте №4
 
 function ensureMaxSpeed(carInfo) {
-  if (!carInfo.hasOwnProperty("maxSpeed")) {
-    carInfo["maxSpeed"] = 255;
-    console.log(`Свойство "maxSpeed" добавлено. Значение: ${carInfo["maxSpeed"]}`);
-  } else {
-    console.log("Свойство 'maxSpeed' уже существует. Ничего не делаем.");
-  }
-  return carInfo;
+    if (!carInfo.hasOwnProperty("maxSpeed")) {
+        carInfo["maxSpeed"] = 255;
+        console.log(`Свойство "maxSpeed" добавлено. Значение: ${carInfo["maxSpeed"]}`);
+    } else {
+        console.log("Свойство 'maxSpeed' уже существует. Ничего не делаем.");
+    }
+    return carInfo;
 }
 
 ensureMaxSpeed(carInfo);
@@ -46,11 +47,11 @@ ensureMaxSpeed(carInfo);
 // функция, получающая первым аргументом — объект, а вторым аргументом — свойство объекта, которое нужно вывести
 
 function getInfo(obj, property) {
-  if (obj && obj.hasOwnProperty(property)) {
-    console.log(obj[property]);
-  } else {
-    console.log(`Свойство "${property}" не найдено`);
-  }
+    if (obj && obj.hasOwnProperty(property)) {
+        console.log(obj[property]);
+    } else {
+        console.log(`Свойство "${property}" не найдено`);
+    }
 }
 
 getInfo(userInfo, 'surname');
@@ -58,7 +59,7 @@ getInfo(userInfo, 'surname');
 
 // Создать массив, содержащий список продуктов
 
-const products = ['Хлеб', 'Молоко', 'Яйца']
+const products = ['Хлеб', 'Молоко', 'Яйца'];
 
 console.log(products);
 
@@ -66,37 +67,34 @@ console.log(products);
 // Создать массив,содержащий книги
 
 const books = [
-{
-  title: '100 самых влиятельных людей в истории',
-  author: 'Майкл Х. Харт',
-  year: 1978,
-  coverColor: 'золотой',
-  genre: 'историческая биография'
-},
-
-{
-  title: 'Великие правители: от Александра до Путина',
-  author: 'Игорь Курукин',
-  year: 2010,
-  coverColor: 'зеленый',
-  genre: 'история'
-},
-
-{
-  title: 'Гении науки: от Ньютона до Хокинга',
-  author: 'Стивен Хокинг',
-  year: 2005,
-  coverColor: 'голубой',
-  genre: 'научная биография'
-},
-
-{
-  title: 'Титаны бизнеса: как они изменили мир',
-  author: 'Эндрю Карнеги',
-  year: 2015,
-  coverColor: 'серый',
-  genre: 'бизнес-биография'
-}
+    {
+        title: '100 самых влиятельных людей в истории',
+        author: 'Майкл Х. Харт',
+        year: 1978,
+        coverColor: 'золотой',
+        genre: 'историческая биография'
+    },
+    {
+        title: 'Великие правители: от Александра до Путина',
+        author: 'Игорь Курукин',
+        year: 2010,
+        coverColor: 'зеленый',
+        genre: 'история'
+    },
+    {
+        title: 'Гении науки: от Ньютона до Хокинга',
+        author: 'Стивен Хокинг',
+        year: 2005,
+        coverColor: 'голубой',
+        genre: 'научная биография'
+    },
+    {
+        title: 'Титаны бизнеса: как они изменили мир',
+        author: 'Эндрю Карнеги',
+        year: 2015,
+        coverColor: 'серый',
+        genre: 'бизнес-биография'
+    }
 ];
 
 // Добавляем книгу в конец
@@ -115,71 +113,62 @@ console.log(books);
 // Серия книг "Семь лучей солнца"
 
 const sunRaysBooks = [
-{
-  title: 'Семь лучей солнца: Книга 1 - Рождение света',
-  author: 'Анна Светлова',
-  year: 2015,
-  coverColor: 'золотистый',
-  genre: 'фэнтези'
-},
-
-{
-  title: 'Семь лучей солнца: Книга 2 - Тени прошлого',
-  author: 'Анна Светлова',
-  year: 2016,
-  coverColor: 'серебряный',
-  genre: 'фэнтези'
-},
-
-{
-  title: 'Семь лучей солнца: Книга 3 - Восход новой эры',
-  author: 'Анна Светлова',
-  year: 2017,
-  coverColor: 'оранжевый',
-  genre: 'фэнтези'
-},
-
-{
-  title: 'Семь лучей солнца: Книга 4 - Закат империи',
-  author: 'Анна Светлова',
-  year: 2019,
-  coverColor: 'красный',
-  genre: 'фэнтези'
-},
-
-{
-  title: 'Семь лучей солнца: Книга 5 - Возвращение героя',
-  author: 'Анна Светлова',
-  year: 2021,
-  coverColor: 'золотой',
-  genre: 'фэнтези'
-}
+    {
+        title: 'Семь лучей солнца: Книга 1 - Рождение света',
+        author: 'Анна Светлова',
+        year: 2015,
+        coverColor: 'золотистый',
+        genre: 'фэнтези'
+    },
+    {
+        title: 'Семь лучей солнца: Книга 2 - Тени прошлого',
+        author: 'Анна Светлова',
+        year: 2016,
+        coverColor: 'серебряный',
+        genre: 'фэнтези'
+    },
+    {
+        title: 'Семь лучей солнца: Книга 3 - Восход новой эры',
+        author: 'Анна Светлова',
+        year: 2017,
+        coverColor: 'оранжевый',
+        genre: 'фэнтези'
+    },
+    {
+        title: 'Семь лучей солнца: Книга 4 - Закат империи',
+        author: 'Анна Светлова',
+        year: 2019,
+        coverColor: 'красный',
+        genre: 'фэнтези'
+    },
+    {
+        title: 'Семь лучей солнца: Книга 5 - Возвращение героя',
+        author: 'Анна Светлова',
+        year: 2021,
+        coverColor: 'золотой',
+        genre: 'фэнтези'
+    }
 ];
 
 // метод спред
 
-  const allBooks = [...books, ...sunRaysBooks];
+const allBooks = [...books, ...sunRaysBooks];
 
-  console.log('Объединенный массив книг');
-  console.log(allBooks);
-  console.log(`Всего книг: ${allBooks.length}`);
+console.log('Объединенный массив книг');
+console.log(allBooks);
+console.log(`Всего книг: ${allBooks.length}`);
 
 // функция сортировки книг по редкости
 
-  function markRareBooks(booksArray) {
+function markRareBooks(booksArray) {
+    return booksArray.map(book => {
+        const newBook = { ...book };
+        newBook.isRare = book.year > 2000;
+        return newBook;
+    });
+}
 
-  return booksArray.map(book => {
+const rareBooks = markRareBooks(allBooks);
 
-  const newBook = { ...book };
-          
-  newBook.isRare = book.year > 2000;
-          
-  return newBook;
-  });
-
-  }
-    
-  const rareBooks = markRareBooks(allBooks)
-
-  console.log('Редкие книги');
-  console.log(rareBooks);
+console.log('Редкие книги');
+console.log(rareBooks);
--- homework-9.js
+++ homework-9.js
@@ -1,3 +1,5 @@
+import { comments } from "./comments.js";
+
 // массив чисел 1-10 с фильтром от 5
 
 let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
@@ -20,13 +22,26 @@ console.log(strings.includes("Тюбетейка"));
 
 // функция переворота
 
+function reverseNumArr(numbers1) {
+  return numbers1.reverse();
+}
 let numbers1 = [1,2,3,4,5,6,7,8,9,10];
-numbers1.reverse();
-console.log(numbers1);
+let invertedNum = reverseNumArr(numbers1)
 
+console.log(invertedNum);
+
+function reverseStringArr(strings1) {
+  return strings1.reverse();
+}
 let strings1 = ["Сивак", "Миск", "Четки", "Книга", "Коврик"]
-strings1.reverse();
+let invertedString = reverseStringArr(strings1)
+
 console.log(strings1);
 
 
-import { comments } from "./comments.js";
+
+// Вывести в консоль массив комментариев юзеров с почтой ".com"
+
+let comEmail = comments.filter(comment => comment.email.includes(".com"));
+
+console.log(comEmail);
